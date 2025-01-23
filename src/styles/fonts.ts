3c80250c51da343e3a23ca7fb1a60e5b26dbe529
Aimad MAJDOU <aimad.majdou@gmail.com>
2025-01-24 00:52:32 +0100

    feat(styling): add different configs for styling

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -3,72 +3,72 @@ import localFont from 'next/font/local';
 export const ibmPlexSans = localFont({
   src: [
     {
-      path: '/assets/fonts/IBMPlexSans-Thin.ttf',
+      path: '../assets/fonts/IBMPlexSans-Thin.ttf',
       weight: '100',
       style: 'normal',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-ThinItalic.ttf',
+      path: '../assets/fonts/IBMPlexSans-ThinItalic.ttf',
       weight: '100',
       style: 'italic',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-ExtraLight.ttf',
+      path: '../assets/fonts/IBMPlexSans-ExtraLight.ttf',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-ExtraLightItalic.ttf',
+      path: '../assets/fonts/IBMPlexSans-ExtraLightItalic.ttf',
       weight: '200',
       style: 'italic',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-Light.ttf',
+      path: '../assets/fonts/IBMPlexSans-Light.ttf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-LightItalic.ttf',
+      path: '../assets/fonts/IBMPlexSans-LightItalic.ttf',
       weight: '300',
       style: 'italic',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-Regular.ttf',
+      path: '../assets/fonts/IBMPlexSans-Regular.ttf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-Italic.ttf',
+      path: '../assets/fonts/IBMPlexSans-Italic.ttf',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-Medium.ttf',
+      path: '../assets/fonts/IBMPlexSans-Medium.ttf',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-MediumItalic.ttf',
+      path: '../assets/fonts/IBMPlexSans-MediumItalic.ttf',
       weight: '500',
       style: 'italic',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-SemiBold.ttf',
+      path: '../assets/fonts/IBMPlexSans-SemiBold.ttf',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-SemiBoldItalic.ttf',
+      path: '../assets/fonts/IBMPlexSans-SemiBoldItalic.ttf',
       weight: '600',
       style: 'italic',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-Bold.ttf',
+      path: '../assets/fonts/IBMPlexSans-Bold.ttf',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '/assets/fonts/IBMPlexSans-BoldItalic.ttf',
+      path: '../assets/fonts/IBMPlexSans-BoldItalic.ttf',
       weight: '700',
       style: 'italic',
     },
@@ -78,10 +78,21 @@ export const ibmPlexSans = localFont({
 export const bebasNeue = localFont({
   src: [
     {
-      path: '/assets/fonts/BebasNeue-Regular.ttf',
+      path: '../assets/fonts/BebasNeue-Regular.ttf',
       weight: '400',
       style: 'normal',
     },
   ],
   variable: '--bebas-neue',
+});
+
+export const notoSansTifinagh = localFont({
+  src: [
+    {
+      path: '../assets/fonts/NotoSansTifinagh-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--noto-sans-tifinagh',
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,22 +10,32 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    // Reorder the breakpoints. By reversing the default order of media queries
+    screens: {
+      '4xl': { max: '1719px' },
+      // => @media (max-width: 1719px) { ... }
+      '2xl': { max: '1419px' },
+      // => @media (max-width: 1419px) { ... }
+      xl: { max: '1259px' },
+      // => @media (max-width: 1259px) { ... }
+      xls: { max: '1179px' },
+      // => @media (max-width: 1179px) { ... }
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+      md: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+      sm: { max: '480px' },
+      // => @media (max-width: 480px) { ... }
+    },
     extend: {
       fontFamily: {
         'ibm-plex-sans': ['IBM Plex Sans', ...fontFamily.sans],
         'bebas-neue': ['var(--bebas-neue)'],
+        'noto-sans-tifinagh': ['var(--noto-sans-tifinagh)'],
       },
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
@@ -34,14 +44,6 @@ export default {
           DEFAULT: 'hsl(var(--secondary))',
           foreground: 'hsl(var(--secondary-foreground))',
         },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
           foreground: 'hsl(var(--destructive-foreground))',
@@ -49,6 +51,10 @@ export default {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
         chart: {
           '1': 'hsl(var(--chart-1))',
           '2': 'hsl(var(--chart-2))',
@@ -56,32 +62,32 @@ export default {
           '4': 'hsl(var(--chart-4))',
           '5': 'hsl(var(--chart-5))',
         },
-        // custom colors
+        // Custom colors
         purple: {
-          100: 'var(--purple-100)',
-          200: 'var(--purple-200)',
-          300: 'var(--purple-300)',
+          100: 'hsl(var(--purple-100))',
+          200: 'hsl(var(--purple-200))',
+          300: 'hsl(var(--purple-300))',
         },
         yellow: {
-          100: 'var(--yellow-100)',
-          200: 'var(--yellow-200)',
+          100: 'hsl(var(--yellow-100))',
+          200: 'hsl(var(--yellow-200))',
         },
         pink: {
-          100: 'var(--pink-100)',
-          200: 'var(--pink-200)',
+          100: 'hsl(var(--pink-100))',
+          200: 'hsl(var(--pink-200))',
         },
         green: {
-          100: 'var(--green-100)',
-          200: 'var(--green-200)',
+          100: 'hsl(var(--green-100))',
+          200: 'hsl(var(--green-200))',
         },
         neutral: {
-          100: 'var(--neutral-100)',
-          200: 'var(--neutral-200)',
-          300: 'var(--neutral-300)',
-          400: 'var(--neutral-400)',
+          100: 'hsl(var(--neutral-100))',
+          200: 'hsl(var(--neutral-200))',
+          300: 'hsl(var(--neutral-300))',
+          400: 'hsl(var(--neutral-400))',
         },
         white: {
-          DEFAULT: 'var(--white)',
+          DEFAULT: 'hsl(var(--white))',
         },
       },
       borderRadius: {
@@ -89,21 +95,89 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
-      screens: {
-        '4xl': { max: '1719px' },
-        // => @media (max-width: 1719px) { ... }
-        '2xl': { max: '1419px' },
-        // => @media (max-width: 1419px) { ... }
-        xl: { max: '1259px' },
-        // => @media (max-width: 1259px) { ... }
-        xls: { max: '1179px' },
-        // => @media (max-width: 1179px) { ... }
-        lg: { max: '1023px' },
-        // => @media (max-width: 1023px) { ... }
-        md: { max: '767px' },
-        // => @media (max-width: 767px) { ... }
-        sm: { max: '480px' },
-        // => @media (max-width: 480px) { ... }
+      spacing: {
+        0.25: '0.0625rem',
+        0.75: '0.1875rem',
+        4.5: '1.125rem',
+        5.5: '1.375rem',
+        6.5: '1.75rem',
+        7.5: '1.875rem',
+        8.5: '2.125rem',
+        9.5: '2.375rem',
+        13: '3.25rem',
+        15: '3.75rem',
+        17: '4.25rem',
+        18: '4.5rem',
+        19: '4.75rem',
+        21: '5.25rem',
+        22: '5.5rem',
+        26: '6.5rem',
+        30: '7.5rem',
+        34: '8.5rem',
+        38: '9.5rem',
+        42: '10.5rem',
+        58: '14.5rem',
+      },
+      fontSize: {
+        0: ['0px', '0px'],
+        sm: ['0.875rem', '1.3125rem'],
+        h1: [
+          '3rem',
+          {
+            lineHeight: '3.5rem',
+            fontWeight: '800',
+          },
+        ],
+        h2: [
+          '2.25rem',
+          {
+            lineHeight: '2.875rem',
+            fontWeight: '800',
+          },
+        ],
+        h3: [
+          '1.875rem',
+          {
+            lineHeight: '2.375rem',
+            fontWeight: '800',
+          },
+        ],
+        h4: [
+          '1.5rem',
+          {
+            lineHeight: '2rem',
+            fontWeight: '800',
+          },
+        ],
+        h5: [
+          '1.25rem',
+          {
+            lineHeight: '1.75rem',
+            fontWeight: '800',
+          },
+        ],
+        h6: [
+          '1.125rem',
+          {
+            lineHeight: '1.5rem',
+            fontWeight: '800',
+          },
+        ],
+      },
+      zIndex: {
+        1: '1',
+        2: '2',
+        3: '3',
+        4: '4',
+        5: '5',
+      },
+      boxShadow: {
+        'primary-4': '0.25rem 0.25rem 0 #000000',
+        'primary-6': '0.375rem 0.375rem 0 #000000',
+        'primary-8': '0.5rem 0.5rem 0 #000000',
+        'secondary-4': '0.25rem -0.25rem 0 #000000',
+        'secondary-6': '0.375rem -0.375rem 0 #000000',
+        'secondary-8': '0.5rem -0.5rem 0 #000000',
       },
     },
   },
