2654396ebdfef20687f2109066e6f0e597cdb89f
Aimad MAJDOU <aimad.majdou@gmail.com>
2025-01-24 00:57:34 +0100

    feat(components): add button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,30 +1,31 @@
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
-
 import { cn } from '@/lib/utils';
 
+// Define shared styles for primary and default variants
+const primaryStyles =
+  'bg-primary text-primary-foreground shadow hover:bg-primary/90';
+
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex h-13 items-center justify-center rounded-sm fill-neutral-100 px-5 font-bold text-neutral-100 transition-colors [&_svg]:pointer-events-none [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        default: primaryStyles,
+        primary: primaryStyles,
+        destructive: 'bg-destructive shadow-sm hover:bg-destructive/90',
+        outline: 'border border-neutral-100',
+        secondary: 'shadow-sm hover:bg-secondary/80',
+        ghost: 'bg-transparent hover:fill-neutral-300 hover:text-neutral-300',
         link: 'text-primary underline-offset-4 hover:underline',
+        purple: 'text-neutral-100 hover:bg-purple-100/90',
       },
       size: {
-        default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
-        icon: 'size-9',
+        default: 'h-13 px-5',
+        sm: 'h-8 text-xs [&_svg]:size-4',
+        md: 'h-9 [&_svg]:size-6',
+        medium: 'h-9 text-xs',
       },
     },
     defaultVariants: {
@@ -43,6 +44,7 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
